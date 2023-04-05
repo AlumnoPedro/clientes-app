61b966cfd45fc937ce868b07f708e7b3d8937603
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-04-05 12:58:34 +0200

    Video 20. Listar Clientes de archivo JSON

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,8 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Pedro', apellido: 'Diaz', email: 'pedro.difer.pdf@gmail.com', createAt: '2023-04-05'},
+    {id: 2, nombre: 'Pedro', apellido: 'Diaz', email: 'pedro.difer.pdf@gmail.com', createAt: '2023-04-05'},
+    {id: 3, nombre: 'Pedro', apellido: 'Diaz', email: 'pedro.difer.pdf@gmail.com', createAt: '2023-04-05'},
+    {id: 4, nombre: 'Pedro', apellido: 'Diaz', email: 'pedro.difer.pdf@gmail.com', createAt: '2023-04-05'},
+];
