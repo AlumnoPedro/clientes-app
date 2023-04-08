ea94df74c61e1f16b1941aa431dce898b35b7aae
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-04-08 12:07:15 +0200

    Video 51. Creacion FormComponent y ruta para el form

---
+++ src/app/clientes/form.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Cliente } from './cliente';
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html'
+})
+export class FormComponent {
+  public cliente: Cliente = new Cliente();
+  public titulo: string = "Crear cliente";
+
+  public create(): void {
+    console.log("Clicked");
+    console.log(this.cliente);
+  }
+
+}
