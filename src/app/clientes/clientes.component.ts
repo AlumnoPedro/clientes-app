8cc0d6348904e314e297afb36f74156dcdcdc829
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-04-15 16:04:42 +0200

    Video 85. Implementacion de la ruta en app module y uso de activatedRoute en el componente.

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -3,28 +3,36 @@ import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 import swal from 'sweetalert2';
 import { tap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
 })
-export class ClientesComponent implements OnInit{
+export class ClientesComponent implements OnInit {
   clientes!: Cliente[];
 
-  constructor(private clienteService: ClienteService){
+  constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute) {
 
   }
 
   ngOnInit() {
-    let page = 0;
-    this.clienteService.getClientes(page).pipe(
-      //Tambien se puede poner el contenido del subscribe en el tap y dejar el subscribe vacio pero no se puede borrar
-      tap(response => {
-        (response.content as Cliente[]).forEach(cliente => {
-          console.log(cliente.nombre);
-        });
-      })
-    ).subscribe(response => this.clientes = response.content as Cliente[]);
+    this.activatedRoute.paramMap.subscribe(params => {
+      //De string a number
+      let page: number = +params.get('page');
+      //Para la primera pagina el parametro page no existe asi que se inicializa a 0
+      if (!page) {
+        page = 0;
+      }
+      this.clienteService.getClientes(page).pipe(
+        //Tambien se puede poner el contenido del subscribe en el tap y dejar el subscribe vacio pero no se puede borrar
+        tap(response => {
+          (response.content as Cliente[]).forEach(cliente => {
+            console.log(cliente.nombre);
+          });
+        })
+      ).subscribe(response => this.clientes = response.content as Cliente[]);
+    });
   }
 
   delete(cliente: Cliente): void {
@@ -35,7 +43,7 @@ export class ClientesComponent implements OnInit{
       },
       buttonsStyling: false
     })
-    
+
     swalWithBootstrapButtons.fire({
       title: 'Esta seguro?',
       text: `Seguro que desea eliminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
