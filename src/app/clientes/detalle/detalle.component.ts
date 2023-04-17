5a5abe021829de18380d55556c55329e9978ac5b
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-04-17 18:42:27 +0200

    Video 105. Validacion de imagen antes de subir y validacion de tipo image.

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -13,7 +13,7 @@ export class DetalleComponent implements OnInit{
 
   cliente: Cliente;
   titulo: string = "Detalle del cliente";
-  private fotoSeleccionada: File; 
+  public fotoSeleccionada: File; 
 
   constructor(private clienteService: ClienteService, private activatedRoute: ActivatedRoute){}
 
@@ -33,13 +33,20 @@ export class DetalleComponent implements OnInit{
   seleccionarFoto(event){
     this.fotoSeleccionada = event.target.files[0];
     console.log(this.fotoSeleccionada);
+    if(this.fotoSeleccionada.type.indexOf('image')<0){
+      swal.fire('Error seleccionar imagen: ', 'El archivo debe ser una imagen', 'error');
+      this.fotoSeleccionada = null;
+    }
   }
 
   subirFoto(){
+    if(!this.fotoSeleccionada){
+      swal.fire('Error Upload: ', 'Debe seleccionar una foto', 'error');
+    } else {
     this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id).subscribe(cliente => {
       this.cliente = cliente;
       swal.fire('La foto se ha subido completamente!', `La foto se ha subido con exito: ${this.cliente.foto}`, 'success');
     });
   }
-
+}
 }
